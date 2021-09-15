a32dca8e2c2b32d0955b68c1d2e3cd3145353c88
Pedro Teles <pedroateles@protonmail.com>
2021-09-15 19:18:12 -0300

    Add code sample of chapter 2.1.1

---
+++ src/main/kotlin/chapters/chapter2/CH2.1.1-HelloWord.kt
@@ -0,0 +1,15 @@
+package chapters.chapter2
+
+fun main(args: Array<String>) {
+    println("Hello, World!")
+}
+
+// >>> Hello, World!
+
+/**
+ * fun keyword is used to declare a function
+ * The parameter type is written after its name (args: Array<String>)
+ * You don't need to put a function inside a Class (can be declared at top level of the file)
+ * println instead of System.out.println
+ * Semicolon is omitted at the end of the line
+ */

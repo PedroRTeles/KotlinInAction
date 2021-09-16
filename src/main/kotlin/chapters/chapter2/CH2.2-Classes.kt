e37895f07fe15ec26e200ddf93ade1316ac822d6
Pedro Teles <pedroateles@protonmail.com>
2021-09-16 14:11:39 -0300

    Add code sample of chapter 2.2

---
+++ src/main/kotlin/chapters/chapter2/CH2.2-Classes.kt
@@ -0,0 +1,21 @@
+package chapters.chapter2
+
+class Person(val name: String)
+
+/**
+ * In Kotlin classes have less boilerplate code. The public keyword was omitted because its the
+ * default visibility
+ * The same class in Java its expressed in the code bellow
+ */
+
+//public class Person() {
+//    private final String name;
+//
+//    public Person(String name) {
+//        this.name = name;
+//    }
+//
+//    public String getName() {
+//        return this.name
+//    }
+//}
